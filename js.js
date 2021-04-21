0b91ab3b968a1439e381ed9d1ec1a95835143f3f
Your Name <madhushreebaligattimath338@gmail.com>
2021-04-22 00:28:29 +0530

    testcode

---
+++ js.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("nav").style.width = "60%" ;
+  document.getElementById("nav").style.height = "60%" ;
+  document.getElementById("nav").style.padding = "30px" ;
+}
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+  document.getElementById("nav").style.border= "0px";
+  document.getElementById("nav").style.padding = "0px" ;
+}
